a72be6efdbe0e48608e73caecdaf3cfe92d132c7
HitzSPB <daniel.oesterbro@gmail.com>
2021-09-26 21:07:05 +0200

    Added courses, but issues with rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/reset.css';
+import './styles/global.css';
+import Courses from './components/courses';
 
 function App() {
   // fetch('http://localhost:5000/api/courses').then(res => res.json()).then((data) => {console.log(data)});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Courses />
   );
 }
 
